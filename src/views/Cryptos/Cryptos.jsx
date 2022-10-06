8a93ab73b9cbbd515c3cb67f9116f303e6822b87
Bilal Goktas <bgoktas97@gmail.com>
2022-10-06 21:15:15 +0200

    added all cryptos without pagination

--- src/views/Cryptos/Cryptos.jsx
+++ src/views/Cryptos/Cryptos.jsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import ListItem from "../../components/ListItem/ListItem";
+import styles from "./Cryptos.module.css";
 
 const Cryptos = () => {
-  return <div>Cryptos</div>;
+  const [cryptos, setCryptos] = useState([]);
+  const [error, setError] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/all")
+      .then((response) => response.json())
+      .then((list) => setCryptos(list))
+      .catch((err) => setError(err))
+      .finally(() => setIsLoaded(true));
+  }, []);
+
+  return (
+    <div className={styles.container}>
+      {!isLoaded ? (
+        <p>Loading...</p>
+      ) : error ? (
+        <p>Error occurred</p>
+      ) : (
+        cryptos.data.map((item) => (
+          <ListItem
+            key={item.id}
+            id={item.id}
+            symbol={item.symbol}
+            convert={"USD"}
+            price={item.quote["USD"].price}
+            change={item.quote["USD"].percent_change_24h}
+            volume={item.quote["USD"].volume_24h}
+          />
+        ))
+      )}
+    </div>
+  );
 };
 
 export default Cryptos;
