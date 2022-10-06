85a3d803dc5e5b2ac1de43eeb47e3d81e34e14b2
Bilal Goktas <bgoktas97@gmail.com>
2022-10-06 19:31:16 +0200

    mapped favs in favorites component

--- src/views/Favorites/Favorites.jsx
+++ src/views/Favorites/Favorites.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useContext } from "react";
+import styles from "./Favorites.module.css";
+import ListItem from "../../components/ListItem/ListItem";
+import { AppContext } from "../../contexts/AppContext";
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const { favCryptos } = useContext(AppContext);
+  return (
+    <div className={styles.container}>
+      {favCryptos.map((item) => (
+        <ListItem
+          id={item.id}
+          symbol={item.symbol}
+          convert={item.convert}
+          price={item.price}
+          change={item.change}
+          volume={item.volume}
+        />
+      ))}
+    </div>
+  );
 };
 
 export default Favorites;
