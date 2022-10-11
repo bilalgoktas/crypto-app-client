af395d554146f41ac8411f459668b52e9dd90fa9
Bilal Goktas <bgoktas97@gmail.com>
2022-10-11 19:16:53 +0200

    removed comment lines

--- src/components/TopList/TopList.jsx
+++ src/components/TopList/TopList.jsx
@@ -22,35 +22,6 @@ const TopList = () => {
         <List data={data} error={error} isLoaded={isLoaded} />
       )}
 
-      {/* <table>
-        <tr>
-          <th></th>
-          <th className={styles.rank}>Rank</th>
-          <th></th>
-          <th className={styles.name}>Name</th>
-          <th className={styles.price}>Price ({currentFiat.symbol})</th>
-          <th className={styles.change}>Change 24h</th>
-          <th className={styles.volume}>Volume ({currentFiat.symbol})</th>
-        </tr>
-        {!isLoaded ? (
-          <td>Loading...</td>
-        ) : error ? (
-          <td>Error occurred</td>
-        ) : (
-          data.data.map((item) => (
-            <ListItem
-              key={item.id}
-              id={item.id}
-              rank={item.cmc_rank}
-              name={item.name}
-              symbol={item.symbol}
-              price={item.quote[currentFiat.name]?.price}
-              change={item.quote[currentFiat.name]?.percent_change_24h}
-              volume={item.quote[currentFiat.name]?.volume_24h}
-            />
-          ))
-        )}
-      </table> */}
       <div className={styles.link}>
         <Link className={styles.linkItem} to="/cryptos">
           See more cryptocurrencies
